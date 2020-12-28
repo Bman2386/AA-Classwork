3d618654d609e71ec9bf866c59b606fd532ae89d
Brendon <brendon.biagi@gmail.com>
2020-12-28 16:57:04 -0500

    add w5d2

---
+++ W11D3/Auth/frontend/components/session/signup_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import {createNewUser} from '../../actions/session';
+import Signup from './signup';
+
+
+const mDTP = dispatch => {
+    return {
+        createNewUser: formUser => dispatch(createNewUser(formUser))
+    }
+};
+
+export default connect(null, mDTP)(Signup);
