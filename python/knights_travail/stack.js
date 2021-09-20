4a24776d02041451549877ee874378e98e539a50
Brendon <brendon.biagi@gmail.com>
2021-09-20 16:58:06 -0400

    add notes

---
+++ python/knights_travail/stack.js
@@ -0,0 +1,32 @@
+/**
+ */
+var CustomStack = function (maxSize) {
+
+};
+
+/** 
+
+ */
+CustomStack.prototype.push = function (x) {
+
+};
+
+/**
+ */
+CustomStack.prototype.pop = function () {
+
+};
+
+/** 
+ */
+CustomStack.prototype.increment = function (k, val) {
+
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
