304a381b98217f6c881f1a95e4e97f7e6249dc48
Brendon <brendon.biagi@gmail.com>
2021-09-29 16:57:45 -0400

    add more practice

--- python/practice.js
+++ python/practice.js
@@ -11,3 +11,20 @@ console.log(Math.pow(2,3)) // 8
 console.log(Math.abs(-2)) // 2
 
 setTimeout(() => {console.log('done')}, 2000) // prints done after 2,000 miliseconds
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => { //promises accept 2 arguments resolve, reject
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+}
+
+asyncCall();
